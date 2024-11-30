54565ad0a753d43bacb7579d51e005f5a41e93b1
Denver Coneybeare <dconeybe@google.com>
2024-11-29 21:59:42 -0500

    buildSrc started

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+  // See https://docs.gradle.org/current/userguide/kotlin_dsl.html#sec:kotlin-dsl_plugin
+  `kotlin-dsl`
+}
+
+java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
+
+dependencies {
+  compileOnly("com.android.tools.build:gradle-api:8.7.2")
+}
